dfd3060866d978ffe8e3ae969f7b7d35fa5b10a5
DreisPa <132481982+DreisPa@users.noreply.github.com>
2024-10-31 06:42:33 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,26 @@
+function submitAnswer(answer) {
+    fetch('/poll', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ answer })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('yesCount').innerText = data.yes || 0;
+        document.getElementById('noCount').innerText = data.no || 0;
+    })
+    .catch(error => console.error('Ошибка:', error));
+}
 
+// Получение начальных данных
+fetch('/poll')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('yesCount').innerText = data.yes || 0;
+        document.getElementById('noCount').innerText = data.no || 0;
+    })
+    .catch(error => console.error('Ошибка:', error));
+
+Найти еще
